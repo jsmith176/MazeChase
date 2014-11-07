7f26cb50bdc8c8693948855f04ea27600f6ce727
Joshua Smith <jsmith_1992@yahoo.com>
2014-11-06 22:52:01 -0500

    ScoreManager and PlayerNoise2 re-upload.

---
+++ MazeChase/MazeChase/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MazeChase
+{
+    class ScoreManager
+    {
+        SpriteFont spriteFont;
+        Vector2 position;
+        string text;
+        int score;
+
+        public ScoreManager(SpriteFont font)
+        {
+            spriteFont = font;
+            position = new Vector2(10, 10);
+            score = 0;
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            text = "Score: " + score;
+            spriteBatch.DrawString(spriteFont, text, new Vector2(position.X - 1, position.Y - 1), Color.Gray);
+            spriteBatch.DrawString(spriteFont, text, position, Color.Yellow);
+        }
+
+        public void increaseScore(int s)
+        {
+            score += s;
+        }
+    }
+}
