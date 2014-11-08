1f1aff642c58d5c1016702fa841afc232339d770
Joshua Smith <jsmith_1992@yahoo.com>
2014-11-08 16:16:44 -0500

    Added Ghost.cs

---
+++ MazeChase/MazeChase/Ghost.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MazeChase
+{
+    class Ghost
+    {
+        Texture2D texture;
+        Vector2 startingPosition;
+        Random rand;
+
+        public Ghost(Texture2D texture, Vector2 startingPosition)
+        {
+            this.texture = texture;
+            this.startingPosition = startingPosition;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            Wander();
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public void Wander()
+        {
+            // Move randomly at intersections
+        }
+
+        public void Flee()
+        {
+            // Move in the direction that increases the distance the most from PacMan
+            // assuming there is no obstacle in that direction
+        }
+
+        public void Chase()
+        {
+            // Move in the direction that decreases the distance the most from PacMan
+            // assuming there is no obstacle in that direction
+        }
+    }
+}
